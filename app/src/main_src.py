b951df6877d2b0e11cc88f20f36bead7fbf65b4b
Mikhail <mtsygikalo@bk.ru>
2024-03-03 15:52:19 +0300

    fix dataset

--- app/src/main_src.py
+++ app/src/main_src.py
@@ -14,9 +14,7 @@ def main_src(data:str):
     vectors = vectorizer.fit_transform(list(words.data_set.keys()))
     
     clf = LogisticRegression()
-    clf.fit(vectors, list(words.data_set.values()))
-
-    del words.data_set   
+    clf.fit(vectors, list(words.data_set.values())) 
 
     text_vector = vectorizer.transform([data]).toarray()[0]
     answer = clf.predict([text_vector])[0]
